24444f2d359578b794382e0cb24d86582ef5c3c5
Waheed Khaled <82209737+Wehda23@users.noreply.github.com>
2023-09-28 11:53:44 +0300

    Update settings.py

--- RealtimeChat/RealtimeChat/settings.py
+++ RealtimeChat/RealtimeChat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cxgu-6i@2ncd5zwrirjx5pe5abfx8q8=gxcqn&41et6jyq44zf'
+SECRET_KEY = *************************************************
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
